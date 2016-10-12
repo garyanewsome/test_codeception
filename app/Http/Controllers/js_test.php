cce5614a8490be57f9e731f1aa2e38f00994f58a
Gary A. Newsome <garyanewsome@gmail.com>
2016-10-12 15:16:52 -0400

    Here we go go go

---
+++ app/Http/Controllers/js_test.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class js_test extends Controller
+{
+    public function setDate(){
+      $date = filter_input(INPUT_POST, 'test_date')
+    }
+
+    public function getDate(){
+      return $date;
+    }
+}
---
+++ app/js_test.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class js_test extends Model
+{
+    public function date(){
+      return $date;
+    }
+}
