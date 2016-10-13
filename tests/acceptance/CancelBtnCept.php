c9f04b0a7e9c2fec52fa2c87c07c52425d6846b9
Gary A. Newsome <garyanewsome@gmail.com>
2016-10-13 15:27:13 -0400

    Successful acceptance test

---
+++ tests/acceptance/CancelBtnCept.php
@@ -0,0 +1,9 @@
+<?php 
+// $I = new AcceptanceTester($scenario);
+// $I->wantTo('Click React input box and then click Cancel button');
+
+// dd("HI!");
+// $I->amOnPage('/');
+// $els = $this->getModule('WebDriver')->_findElements('input');
+// $I->click(input.first);
+// $I->click('button' => 'Cancel');
---
+++ tests/acceptance/CancelBtnCest.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class CancelBtnCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        // $I->waitPageLoad();
+        $I->wantTo('Click React input box and then click Cancel button');
+        $I->amOnPage('/');
+        $I->see('Laravel');
+        $I->seeElement('input', ['value' => '01/01/2018 2:46 PM']);
+
+        // $els = $this->getModule('WebDriver')->_findElements('input');
+        
+
+        // $I->click(input.first);
+        // $I->click('button' => 'Cancel');
+    }
+}
